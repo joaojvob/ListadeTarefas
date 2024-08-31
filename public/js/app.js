04b5f1e827dcf8a2306987946568888c6c5f8698
joao <joaojuliovob@gmail.com>
2024-08-31 17:43:02 -0300

    .

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-dt/css/jquery.dataTables.css';
+
+$(document).ready(function() {
+    $('#tasksTable').DataTable();
+});
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,22 +2,44 @@
 <html>
 <head>
     <title>Lista de Tarefas</title>
+    
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+
 </head>
 <body>
-    <h1>Lista de Tarefas</h1>
-    <a href="{{ route('tasks.create') }}">Criar Nova Tarefa</a>
-    <ul>
-        @foreach($tasks as $task)
-            <li>
-                {{ $task->title }}
-                <a href="{{ route('tasks.edit', $task) }}">Editar</a>
-                <form action="{{ route('tasks.destroy', $task) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Excluir</button>
-                </form>
-            </li>
-        @endforeach
-    </ul>
+    <div class="d-flex justify-content-center container">
+        <h1>Lista de Tarefas</h1>
+        <a href="{{ route('tasks.create') }}" class="btn btn-primary">Criar Nova Tarefa</a>
+        <table id="tasksTable" class="display">
+            <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($tasks as $task)
+                <tr>
+                    <td>{{ $task->title }}</td>
+                    <td>
+                        <a href="{{ route('tasks.edit', $task) }}" class="btn btn-warning">Editar</a>
+                        <form action="{{ route('tasks.destroy', $task) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="delete btn btn-danger">Excluir</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 </body>
 </html>
