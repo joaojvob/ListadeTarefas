b8d5f0148cff263df0c8451476b64e4b44b5aa9c
joao <joaojuliovob@gmail.com>
2024-08-31 15:18:46 -0300

    melhorias na implementacao

---
+++ resources/js/app.js
@@ -0,0 +1,31 @@
+import './bootstrap';
+
+const mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js')
+   .css('resources/css/app.css', 'public/css');
+
+$(document).ready(function() {
+    $('#minhaTabela').DataTable();
+});
+
+function adicionarItem() {
+    debugger
+    alert('Adicionar Item clicado!');
+}
+
+function verItem(button) {
+   
+}
+
+function editarItem(button) {
+    
+}
+
+function excluirItem(button) {
+    if (confirm('Tem certeza que deseja excluir este item?')) {
+        const row = $(button).closest('tr');
+        $('#minhaTabela').DataTable().row(row).remove().draw();
+        alert('Item excluído!');
+    }
+}
